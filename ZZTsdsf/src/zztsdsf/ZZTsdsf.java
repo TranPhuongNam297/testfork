6c57bf91cd5ea7828bacdb81ae3027550b68c5af
TranPhuongNam297 <namtran0108642@gmail.com>
2023-06-16 12:21:46 +0700

    test

--- ZZTsdsf/src/zztsdsf/ZZTsdsf.java
+++ ZZTsdsf/src/zztsdsf/ZZTsdsf.java
@@ -14,7 +14,7 @@ public class ZZTsdsf {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.err.println("Hello World!");
     }
     
 }
